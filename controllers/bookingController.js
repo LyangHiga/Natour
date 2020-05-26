d0ead44f7b913fd812433257305f0c9d3f7cb10c
lyangHiga <lyang_higa@poli.ufrj.br>
2020-05-26 17:52:11 -0300

    Removing Comments

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -12,40 +12,6 @@ const {
   getAll,
 } = require('./handlerFactory');
 
-// exports.getCheckoutSession = catchAsync(async (req, res, next) => {
-//   // get booked tour
-//   const tour = await Tour.findById(req.params.tourId);
-
-//   // 2) Create checkout session
-//   const session = await stripe.checkout.sessions.create({
-//     payment_method_types: ['card'],
-//     // success_url: `${req.protocol}://${req.get('host')}/?tour=${
-//     //   req.params.tourId
-//     // }&user=${req.user.id}&price=${tour.price}`,
-//     success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
-//     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
-//     customer_email: req.user.email,
-//     client_reference_id: req.params.tourId,
-//     line_items: [
-//       {
-//         name: `${tour.name} Tour`,
-//         description: tour.summary,
-//         images: `${req.protocol}://${req.get('host')}/img/tours/${
-//           tour.imageCover
-//         }`,
-//         amount: tour.price * 100,
-//         currency: 'usd',
-//         quantity: 1,
-//       },
-//     ],
-//   });
-//   // 3) Create session as response
-//   res.status(200).json({
-//     status: 'success',
-//     session,
-//   });
-// });
-
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // get booked tour
   const tour = await Tour.findById(req.params.tourId);
