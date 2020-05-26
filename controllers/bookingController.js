7cf4ce38f00e056d0e3796c65e335f272056ac69
lyangHiga <lyang_higa@poli.ufrj.br>
2020-05-26 17:49:06 -0300

    Image checkout session correction

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -12,6 +12,40 @@ const {
   getAll,
 } = require('./handlerFactory');
 
+// exports.getCheckoutSession = catchAsync(async (req, res, next) => {
+//   // get booked tour
+//   const tour = await Tour.findById(req.params.tourId);
+
+//   // 2) Create checkout session
+//   const session = await stripe.checkout.sessions.create({
+//     payment_method_types: ['card'],
+//     // success_url: `${req.protocol}://${req.get('host')}/?tour=${
+//     //   req.params.tourId
+//     // }&user=${req.user.id}&price=${tour.price}`,
+//     success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
+//     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+//     customer_email: req.user.email,
+//     client_reference_id: req.params.tourId,
+//     line_items: [
+//       {
+//         name: `${tour.name} Tour`,
+//         description: tour.summary,
+//         images: `${req.protocol}://${req.get('host')}/img/tours/${
+//           tour.imageCover
+//         }`,
+//         amount: tour.price * 100,
+//         currency: 'usd',
+//         quantity: 1,
+//       },
+//     ],
+//   });
+//   // 3) Create session as response
+//   res.status(200).json({
+//     status: 'success',
+//     session,
+//   });
+// });
+
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // get booked tour
   const tour = await Tour.findById(req.params.tourId);
@@ -19,9 +53,6 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 2) Create checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    // success_url: `${req.protocol}://${req.get('host')}/?tour=${
-    //   req.params.tourId
-    // }&user=${req.user.id}&price=${tour.price}`,
     success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
@@ -30,9 +61,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
       {
         name: `${tour.name} Tour`,
         description: tour.summary,
-        images: `${req.protocol}://${req.get('host')}/img/tours/${
-          tour.imageCover
-        }`,
+        images: [
+          `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`,
+        ],
         amount: tour.price * 100,
         currency: 'usd',
         quantity: 1,
